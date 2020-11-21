74b19da66d51485949af8351dc552fdcc486fb16
Moti Elmakies <motiphone2003@gmail.com>
2020-11-21 20:33:14 +0200

    :apple: (misc) basic implementation completed,  refacture to

--- src/components/Tag_V1.jsx
+++ src/components/Tag_V1.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 const Tag = () => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const [tag, setTag] = useState('dog');
+  const [gif, setGif] = useState('');
+
+  const handleClick = () => {
+    getData(tag);
+  };
+
+  const getData = async (tag) => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+    const { data } = await axios.get(url);
+    const imageSrc = data.data.images.downsized_large.url;
+    setGif(imageSrc);
+  };
+
+  useEffect(() => {
+    getData(tag);
+  }, [tag]);
+
   return (
-    <React.Fragment>
-      <h1>Tag</h1>
-    </React.Fragment>
+    <div className='container'>
+      <h1>Random {tag} GIF</h1>
+      <img width='500' src={gif} alt='Random gif'></img>
+      <input value={tag} onChange={(e) => setTag(e.target.value)} />
+      <button onClick={handleClick}>Click for new</button>
+    </div>
   );
 };
 
