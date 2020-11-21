977e6a1804dd68377d9d66bf7f38b313505cd723
Moti Elmakies <motiphone2003@gmail.com>
2020-11-21 15:40:19 +0200

    :gear: (feature) add random gif

---
+++ src/components/Random_V1.jsx
@@ -0,0 +1,33 @@
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+const Random = () => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const [gif, setGif] = useState('');
+
+  const handleClick = () => {
+    getData();
+  };
+
+  const getData = async () => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const { data } = await axios.get(url);
+    const imageSrc = data.data.images.downsized_large.url;
+    setGif(imageSrc);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className='container'>
+      <h1>Random GIF</h1>
+      <img width='500' src={gif} alt='Random gif'></img>
+      <button onClick={handleClick}>Click for new</button>
+    </div>
+  );
+};
+
+export default Random;
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Random from './Random_V1';
+import '../app.css';
+import Tag from './Tag_V1';
+
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <h1>Random GIF Application</h1>
+      <div className='main-container'>
+        <Random />
+        <Tag />
+      </div>
     </div>
   );
 };
